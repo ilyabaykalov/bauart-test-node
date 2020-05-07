23ba03e283df1f061dd30b12ebae86230a041e29
Ilya Baykalov <enot_boris@icloud.com>
2020-05-07 09:14:36 +0700

    настроен конфиг

--- config/config.js
+++ config/config.js
@@ -1,24 +1,22 @@
 const options = {
-    promiseLib: require('bluebird')
+	promiseLib: require('bluebird')
 };
 
 const pgp = require('pg-promise')(options);
 
 pgp.pg.types.setTypeParser(1114, function (stringValue) {
-    console.log(stringValue);
-    return new Date(Date.parse(stringValue + "+0000"));
+	console.log(stringValue);
+	return new Date(Date.parse(stringValue + '+0000'));
 });
 
 const config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'o_kids',
-    user: 'postgres',
-    password: 'root'
+	host: 'localhost',
+	port: 5432,
+	database: 'journal',
+	user: 'postgres',
+	password: 'root'
 };
 
 const db = pgp(config);
 
-module.exports = {
-    pgp, db
-};
+module.exports = { pgp, db };
