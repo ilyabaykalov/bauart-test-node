e08702ef7386cd135eac1f9b67a0cffe6bbdf3dc
Ilya Baykalov <enot_boris@icloud.com>
2020-05-07 09:14:23 +0700

    добавлены api для получения данных о главах и уроках

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 let router = require('express').Router();
 
 router.get('/', function (req, res) {
-    res.render('index', {title: 'Journal server'})
+	res.render('index', { title: 'Journal server' });
 });
 
 router.get('/test', function (req, res) {
-    res.send('<h1>simple text</h1>')
+	res.send('<h1>simple text</h1>');
 });
 
-// let account = require('../query/account');
+let chapter = require('../query/chapter'),
+	colors = require('../query/colors');
 
-// router.get('/api/', account.createAccount());
+router.get('/chapters/get', chapter.getAllChapters);
+router.get('/chapters', chapter.getAllData);
+router.post('/chapters', chapter.addChapter);
+
+router.get('/colors', colors.getColors);
 
 module.exports = router;
