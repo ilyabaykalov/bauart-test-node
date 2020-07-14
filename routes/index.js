1ab951693fdadc52d0eea7a97670342c2f23e15d
Ilya Baykalov <enot_boris@icloud.com>
2020-07-14 18:03:18 +0700

    Выход из чата

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-let router = require('express').Router();
-let redis;
+const router = require('express').Router();
+const io = require('socket.io')();
+const redis = require('redis').createClient();
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Bauart test server' });
@@ -11,19 +12,7 @@ router.post('/apod', NASA.addAPOD);
 router.post('/earth', NASA.addEarth);
 router.post('/mars', NASA.addMars);
 
-const io = require('socket.io')();
-
 io.on('connection', (socket) => {
-  console.log('Connected to Socket.io...');
-  redis = require('redis').createClient();
-  redis.on('connect', () => {
-    console.log('Connected to Redis...');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected from Socket.io...');
-  });
-
   socket.on('userJoined', (user) => {
     socket.join(user.room);
 
@@ -54,34 +43,15 @@ io.on('connection', (socket) => {
 
     io.sockets.to(message.room).emit('message', response);
   });
-});
 
-// const addMessageToRedis = (room, message) => {
-// redis.set(room, 'Hello Redis', function(err, repl) {
-//   if (err) {
-//     // Оо что то случилось при записи
-//     console.log('Что то случилось при записи: ' + err);
-//     redis.quit();
-//   } else {
-//     // Прочтем записанное
-//     redis.get('myKey', function(err, repl) {
-//       //Закрываем соединение, так как нам оно больше не нужно
-//       redis.quit();
-//       if (err) {
-//         console.log('Что то случилось при чтении: ' + err);
-//       } else if (repl) {
-//         // Ключ найден
-//         console.log('Ключ: ' + repl);
-//       } else {
-//         // Ключ ненайден
-//         console.log('Ключ ненайден.');
-//
-//       }
-//       ;
-//     });
-//   }
-//   ;
-// });
-// };
+  socket.on('logout', (user) => {
+    socket.broadcast
+        .to(user.room)
+        .emit('message', {
+          name: user.name,
+          text: `вышел из ${ user.room }`
+        });
+  });
+});
 
 module.exports = { router, io };
