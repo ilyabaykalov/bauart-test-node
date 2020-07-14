f1823f3d547e0d13ab03ef75bdf355c2e9022e35
Ilya Baykalov <enot_boris@icloud.com>
2020-07-14 21:57:40 +0700

    чистка проекта

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
 const index = require('./routes/index');
 const indexRouter = index.router;
-const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const cors = require('cors');
-app.use(cors());
-
 app.io = index.io;
 
-app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('view engine', 'pug');
 
+app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use('/', indexRouter);
 
-const createError = require('http-errors');
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 module.exports = app;
+
